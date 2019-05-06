3661faa60eede8e464ced19c2d8edd680fc1de9b
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-05-06 11:38:14 -0700

    added basic project layout and images

---
+++ src/components/projects.js
@@ -0,0 +1,23 @@
+import React from "react"
+import randomQuoteImg from "../images/RandomQuote2.png"
+import JavaScriptCalc from "../images/JavaScriptCalc.png"
+import GuessGaming from "../images/GuessingGame.png"
+import Pomodoro from "../images/PomodoroClock.png"
+import TwitchLayout from "../images/TwitchAPI.png"
+import TicTac from "../images/TicTac.png"
+
+const Projects = () => (
+  <div>
+    <h2>Projects</h2>
+    <div>
+      <img src={randomQuoteImg} />
+      <img src={JavaScriptCalc} />
+      <img src={GuessGaming} />
+      <img src={Pomodoro} />
+      <img src={TwitchLayout} />
+      <img src={TicTac} />
+    </div>
+  </div>
+)
+
+export default Projects
