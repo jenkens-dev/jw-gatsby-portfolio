0d7e66f1460c31d2163428e66b79ab2a63572c55
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-05-07 12:14:45 -0700

    added styles and colors to contact icons

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -3,14 +3,15 @@ import { FaTwitterSquare } from "react-icons/fa"
 import { FaLinkedin } from "react-icons/fa"
 import { GoMail } from "react-icons/go"
 import { FaGithub } from "react-icons/fa"
+import { IconContext } from "react-icons"
 
 const Contact = () => (
   <div>
     <h2>Contact Me</h2>
-    <GoMail />
-    <FaTwitterSquare />
-    <FaLinkedin />
-    <FaGithub />
+    <GoMail style={{ color: "black" }} />
+    <FaTwitterSquare style={{ color: "#1da1f2" }} />
+    <FaLinkedin style={{ color: "#0073b1" }} />
+    <FaGithub style={{ color: "#24292e" }} />
   </div>
 )
 
