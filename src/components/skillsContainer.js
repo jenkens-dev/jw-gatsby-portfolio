ca393574bde2f7c6c1ec2c03199b02f8dec677d8
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-05-11 11:29:35 -0700

    added skillsContainer grid around skills icons

--- src/components/skillsContainer.js
+++ src/components/skillsContainer.js
@@ -5,15 +5,16 @@ import { FaGithub } from "react-icons/fa"
 import { FaCss3Alt } from "react-icons/fa"
 import { FaHtml5 } from "react-icons/fa"
 import { FaReact } from "react-icons/fa"
+import "./styles.css"
 
 const SkillsContainer = () => (
-  <div>
-    <FaJsSquare style={{ color: "#F0DB4F" }} />
-    <FaReact style={{ color: "#61dafb" }} />
-    <FaHtml5 style={{ color: "#E44D26" }} />
-    <FaCss3Alt style={{ color: "#1572B6" }} />
-    <FaGitSquare style={{ color: "#f14e32" }} />
-    <FaGithub style={{ color: "#24292e" }} />
+  <div className="skillsContainer">
+    <FaJsSquare className="skillsIcon" style={{ color: "#F0DB4F" }} />
+    <FaReact className="skillsIcon" style={{ color: "#61dafb" }} />
+    <FaHtml5 className="skillsIcon" style={{ color: "#E44D26" }} />
+    <FaCss3Alt className="skillsIcon" style={{ color: "#1572B6" }} />
+    <FaGitSquare className="skillsIcon" style={{ color: "#f14e32" }} />
+    <FaGithub className="skillsIcon" style={{ color: "#24292e" }} />
   </div>
 )
 
