f899f769f889ceb4bbe7411fe852f98f453ae5ba
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-05-16 21:47:40 -0700

    styled contact icons

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -8,10 +8,14 @@ import { IconContext } from "react-icons"
 const Contact = () => (
   <div>
     <h2>Contact Me</h2>
-    <GoMail style={{ color: "black" }} />
-    <FaTwitterSquare style={{ color: "#1da1f2" }} />
-    <FaLinkedin style={{ color: "#0073b1" }} />
-    <FaGithub style={{ color: "#24292e" }} />
+    <div className="contactGrid">
+      <GoMail style={{ color: "black", height: "90px", width: "90px" }} />
+      <FaTwitterSquare
+        style={{ color: "#1da1f2", height: "90px", width: "90px" }}
+      />
+      <FaLinkedin style={{ color: "#0073b1", height: "90px", width: "90px" }} />
+      <FaGithub style={{ color: "#24292e", height: "90px", width: "90px" }} />
+    </div>
   </div>
 )
 
