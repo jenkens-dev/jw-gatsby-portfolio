d4ebcea12ad0ebbdb1a5d371f8410af7f50fe17a
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-05-24 17:45:09 -0700

    added navbar styles and changed responsive contact section

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,11 +2,15 @@ import React from "react"
 
 const NavBar = () => (
   <div className="navBar">
-    <a className="favicon">Insert JW Favicon Here</a>
+    <a id="title" href="#top">
+      JW
+    </a>
     <div className="pageTitles">
       <a href="#about">About</a>
       <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
+      <a id="navContact" href="#contact">
+        Contact
+      </a>
     </div>
   </div>
 )
